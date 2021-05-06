03e9bf35f6952b22f4fb7471393e05ef861e6c3b
Frederico Wu <fredericowu@gmail.com>
2021-05-05 22:53:31 -0400

    update

--- app/bot/tasks.py
+++ app/bot/tasks.py
@@ -3,13 +3,14 @@ from celery import shared_task
 #from celery.task import periodic_task
 from bot.connector.telegrambot import TelegramBot
 from addon.models import XboxFriends
-
+import anki_vector
 from datetime import timedelta
 import os
 import time
+from anki_vector.connection import ControlPriorityLevel
+from anki_vector.util import distance_mm, speed_mmps, degrees, Angle, Pose
 
 bot = TelegramBot()
-
 
 #
 #def add(celery):
@@ -18,7 +19,7 @@ bot = TelegramBot()
 def check_status():
     fred_id = 1223642032
     notify_friends = ["Broriz4927", ]
-    alert_cmd = '/home/ubuntu/workspace/vector-go-sdk/custom/speak.sh "Lulu is playing, he is playing, he is playing" &'
+    #alert_cmd = '/home/ubuntu/workspace/vector-go-sdk/custom/speak.sh "Lulu is playing, he is playing, he is playing" &'
     status = XboxFriends.check_status()
     for friend in status:
         if status[friend]["changed"] and status[friend]["gamertag"] in notify_friends:
@@ -26,9 +27,26 @@ def check_status():
             bot.connection.send_message(fred_id, msg)
 
             if status[friend]["state"].lower() == "online":
-                os.system(alert_cmd)
-                time.sleep(60)
-                os.system(alert_cmd)
+                for _ in range(3):
+                    try:
+                        with anki_vector.Robot(behavior_activation_timeout=30,
+                                               behavior_control_level=ControlPriorityLevel.OVERRIDE_BEHAVIORS_PRIORITY) as robot:
+                            robot.behavior.drive_off_charger()
+                            robot.behavior.say_text("Lulu is playing")
+                            robot.anim.play_animation_trigger('GreetAfterLongTime')
+                            robot.world.connect_cube()
+                            robot.behavior.drive_straight(distance_mm(200), speed_mmps(100))
+                            robot.behavior.say_text("he is playing")
+                            robot.behavior.turn_in_place(degrees(70))
+                            robot.behavior.say_text("Lulu is playing")
+                            robot.behavior.turn_in_place(degrees(220))
+                            robot.anim.play_animation_trigger('GreetAfterLongTime')
+                            robot.behavior.drive_on_charger()
+                    except anki_vector.exceptions.VectorTimeoutException:
+                        time.sleep(20)
+                        continue
+                    else:
+                        break
 
 
 """
